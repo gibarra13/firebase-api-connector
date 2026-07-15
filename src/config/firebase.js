e66275917309b05b197a4c8e240bbe6614e54176
gabriel ibarra <g.ibarra@ivandrive.com>
2026-07-15 11:28:13 -0400

    chore update firebase-admin to 14.1.0

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,23 @@
-var admin = require("firebase-admin");
+const admin = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const { getDatabase } = require("firebase-admin/database");
+const { getRemoteConfig } = require("firebase-admin/remote-config");
 
 // Fetch the service account key JSON file contents
-var serviceAccount = require("./ivan-drive-firebase-adminsdk-o379n-a67f31685f.json");
+const serviceAccount = require("./ivan-drive-firebase-adminsdk-o379n-a67f31685f.json");
 
 // Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // The database URL depends on the location of the database
-  databaseURL: "https://ivan-drive.firebaseio.com/"
-});
+if (!admin.getApps().length) {
+  admin.initializeApp({
+    credential: admin.cert(serviceAccount),
+    // The database URL depends on the location of the database
+    databaseURL: "https://ivan-drive.firebaseio.com/"
+  });
+}
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var rc = admin.remoteConfig();
-var auth = admin.auth();
+const db = getDatabase();
+const rc = getRemoteConfig();
+const auth = getAuth();
+
 module.exports = { admin, db, rc, auth };
